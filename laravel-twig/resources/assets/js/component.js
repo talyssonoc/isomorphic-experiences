88e8bb58dcbfb25ff84f8113fb1925756c5bf88f
giulianovarriale <giuliano.oca@gmail.com>
2015-09-28 10:37:44 -0300

    DRY and separate concerns. List components in separate file.

--- laravel-twig/resources/assets/js/component.js
+++ laravel-twig/resources/assets/js/component.js
@@ -1,31 +1,19 @@
 define([
   'jquery',
   'twig',
-  'text!components/my_component',
-  'text!components/list_product',
-  'text!components/product',
+  'list_components'
 ],
 function(
   $,
   Twig,
-  myComponent,
-  listProduct,
-  product
+  components
 ) {
 
-  var components = {
-    myComponent: myComponent,
-    listProduct: listProduct,
-    product: product
-  };
-
-  Twig.extendFunction('component', function(name, data) {
-    return Twig.twig({data: components[name]}).render(data);
-  });
-
-  var component = function(component, data) {
+  var component = function(name, data) {
     return Twig.twig({data: components[name]}).render(data);
   };
+
+  Twig.extendFunction('component', component);
 
   return component;
 });
